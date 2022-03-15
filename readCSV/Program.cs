9c55175d4435b74d0e0747c980f3e748ca1242b0
André Miranda <andrelmmiranda@gmail.com>
2022-03-15 10:36:06 -0300

    initial commit

---
+++ readCSV/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace readCSV
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader streamReader = new StreamReader(File.OpenRead(@"C:\Users\aluno10.TSACBRRJLP041\Downloads\transferoacademy_transações2.csv"));
+
+            //Console.WriteLine(streamReader.ReadLine());
+            
+            string line;
+            string[] separated;
+
+            List<Transaction> list = new List<Transaction>();
+
+            int index = 0;
+
+            
+            while (true)
+            {
+                line = streamReader.ReadLine();
+
+                //Console.WriteLine($"{index}: {line}");
+                index++;
+
+                if (line == null) break;
+
+                separated = line.Split(';');
+
+                if (!separated[0].Contains("horario"))
+                {
+                    Transaction t = new Transaction(
+                                            dateTime: separated[0],
+                                            acount: separated[1],
+                                            operationType: separated[2],
+                                            coinCameIn: separated[3],
+                                            valueCameIn: separated[4],
+                                            coinCameOut: separated[5],
+                                            valueCameOut: separated[6]
+                                        );
+
+                    list.Add(t);
+                   // Console.WriteLine($"{t.DateTime}: {t.Acount}: {t.ValueCameOut}");
+                }
+                
+
+            }
+            //"conta da Binance"
+            //"29/12/2021 15:44"
+            //"26/11/2020 00:33"
+            //"conta da FTX"
+            //"deposit"
+            IEnumerable<Transaction> filtered = list.Where(item => item.Acount.Contains("conta da Binance"));
+          
+
+            
+            foreach (Transaction t in filtered)
+            {
+                Console.WriteLine(t.ToString());
+                Console.WriteLine(t.ValueCameOut.GetType());
+                Console.WriteLine();
+            }
+
+
+
+            Console.WriteLine("Acabou");
+            
+
+          
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ readCSV/Transaction.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace readCSV
+{
+    class Transaction
+    {
+
+        public string DateTime { get; private set; }
+        public string Acount { get; private set; }
+        public string OperationType { get; private set; }
+        public string CoinCameIn { get; private set; }
+        public double ValueCameIn { get; set; }
+        public string CoinCameOut { get; private set; }
+        public double ValueCameOut { get; set; }
+
+        public Transaction(string dateTime, string acount, string operationType, string coinCameIn, string valueCameIn, string coinCameOut, string valueCameOut)
+        {
+            DateTime = dateTime;
+            Acount = acount;
+            OperationType = operationType;
+            CoinCameIn = coinCameIn;
+            ValueCameIn = ToDouble(valueCameIn);
+            CoinCameOut = coinCameOut;
+            ValueCameOut = ToDouble(valueCameOut);
+
+        }
+
+        public override string ToString()
+        {
+            return string.Format($"{DateTime} - {Acount} - {OperationType} - {CoinCameIn} - {ValueCameIn} - {CoinCameOut} - {ValueCameOut}");
+        }
+
+        public static double ToDouble(string value)
+        {
+            if (value.Contains(","))
+            {
+                return double.Parse(value, CultureInfo.CreateSpecificCulture("pt-BR"));
+            }
+            else
+            {
+               return double.Parse(value, CultureInfo.InvariantCulture);
+            }
+        }
+    }
+}
